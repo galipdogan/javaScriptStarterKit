91c0b1cef70a288c91c0eccc9202a03917136bd8
galipdogan <galipdogan@gmail.com>
2021-11-23 13:56:11 +0300

    (JavaScript) 4. gün JavaScript ES5+ & OOP

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -1,5 +1,6 @@
 import { BaseLogger, ElasticLogger, MongoLogger } from "../crossCuttingConcerns/logger.js";
 import User from "../models/user.js";
+import Customer from "../models/customer.js"
 import UserService from "../services/userService.js";
 
 console.log(`User Component yuklendi`);
@@ -10,12 +11,10 @@ let userService = new UserService(logger1)
 let user1=new User(1,`Engin`,`Demirog`,`Ankara`)
 let user2=new User(2,`Galip`,`Dogan`,`Izmir`)
 userService.add(user1)
-userService.add(user2)
+userService.add(user2)  /*Kullanıcıların eklendiği sayfa */
 
-console.log(userService.list());
-console.log(userService.getById(2)); 
-
-
+//console.log(userService.list());/*Kullanıcıların listelendiği sayfa */
+//console.log(userService.getById(2)); /*Kullanıcının detayının getirildiği sayfa */
 
 
 
@@ -24,4 +23,14 @@ let customer = {id:1, firstName:`Engin`}
 //prototyping -- sonradan deger ekleme yapabilirsin.
 customer.lastName=`Demirog`
 
-console.log(customer);
+console.log(`------------------------------------`);
+userService.load();
+
+let customerToAdd =  new Customer(1,"Seda","Yılmaz","Ankara","dfdfd")
+customerToAdd.type="customer"
+
+userService.add(customerToAdd)
+console.log(userService.customers);
+console.log(userService.employees);
+console.log(userService.errors);
+console.log(userService.getCustomersSorted())
---
+++ src/data/users.js
@@ -0,0 +1,62 @@
+export const users=[
+    {
+        id:1,
+        firstName:"Engin",
+        lastName:"Demiroğ",
+        city:"Ankara",
+        age:36,
+        creditCardNumber:"123456",
+        type:"customer"
+    },
+    {
+        id:2,
+        firstName:"Mustafa",
+        lastName:"Sansar",
+        city:"Istanbul",
+        age:21,
+        creditCardNumber:"654321",
+        type:"customer"
+
+    },
+    {
+        id:3,
+        firstName:"Cansu",
+        lastName:"Degirmen",
+        city:"Istanbul",
+        age:20,
+        salary:8000,
+        type:"employee"
+
+    },
+    {
+        id:4,
+        firstName:"Mücahit",
+        lastName:"Kaya",
+        city:"Bolu",
+        age:23,
+        salary:8500,
+        type:"employee"
+
+    },
+    {
+        id:5,
+        firstName:"Eren",
+        lastName:"Arı",
+        city:"Diyarbakır",
+        age:20,
+        salary:15000,
+        type:"employee"
+
+    },
+    {
+        id:6,
+        firstName:"Beytullah",
+        lastName:"Zor",
+        city:"Ankara",
+        age:20,
+        salary:15000,
+        type:"employee"
+
+    }
+
+]
